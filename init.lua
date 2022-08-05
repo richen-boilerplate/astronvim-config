e6e7c78c0c466633f8cba3532e665fbcdd77e99c
richen604 <56615615+richen604@users.noreply.github.com>
2022-08-05 15:45:13 -0700

    added transparent plugin

--- init.lua
+++ init.lua
@@ -93,6 +93,27 @@ local config = {
       --     require("lsp_signature").setup()
       --   end,
       -- },
+      {
+        "xiyaowong/nvim-transparent",
+        "",
+        config = function()
+          require("transparent").setup {
+            enable = true, -- boolean: enable transparent
+            extra_groups = { -- table/string: additional groups that should be cleared
+              -- In particular, when you set it to 'all', that means all available groups
+
+              -- example of akinsho/nvim-bufferline.lua
+              "BufferLineTabClose",
+              "BufferlineBufferSelected",
+              "BufferLineFill",
+              "BufferLineBackground",
+              "BufferLineSeparator",
+              "BufferLineIndicatorSelected",
+            },
+            exclude = {}, -- table: groups you don't want to clear
+          }
+        end,
+      },
     },
     -- All other entries override the setup() call for default plugins
     ["null-ls"] = function(config)
